33fe2ddd25f699828bfe1a4dd0073c298ecabdfc
Douglas Freitas <freitas28douglas@gmail.com>
2022-03-14 20:52:40 -0300

    up

---
+++ Lista_loops/ex_03.py
@@ -0,0 +1,5 @@
+valor = int(input('Informe um valor: '))
+contador = 0
+for c in range(1, valor +1):
+    contador = contador + c 
+print('O resultado da soma dos numeros até o valor informado é: {}'.format(contador))
---
+++ Lista_loops/ex_04.py
@@ -0,0 +1,8 @@
+numero = int(input('Informe um número para a tabuada: '))
+for multi in range(1, 11):
+    tabuada = multi * numero
+    print('{} X {} = {}'.format(numero, multi, tabuada))
+
+
+
+    
