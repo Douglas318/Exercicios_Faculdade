9dee4fb9f67a6a9a981b67a3ed25e12c8a8c3c6c
Douglas Freitas <freitas28douglas@gmail.com>
2022-03-15 23:55:08 -0300

    Update ex_08.py

--- Lista_loops/ex_08.py
+++ Lista_loops/ex_08.py
@@ -1,12 +1,15 @@
-notas = []
+notas = [ ]
 nota = 1
 while nota != 0:
-    nota = float(input('Continue informando as notas dos alunos: '))
-    notas.append(nota)
+     nota =(float(input(f"Digite o valor da nota:")))
+     notas.append(nota)
 soma = 0
 for nota in notas:
     soma = soma + nota
-nota = notas [:-1]
+notas=notas[:-1]
 media = soma / len(notas)
+print(f"O valor da média é {round(media,2)}")
 
-print('A média da turma é {}'.format(notas))
+
+
+
