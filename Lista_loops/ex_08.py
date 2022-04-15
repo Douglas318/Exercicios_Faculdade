d2d44c3c68f79bac687ed2cd45313b68af178640
Douglas Freitas <freitas28douglas@gmail.com>
2022-04-15 00:39:15 -0300

    Update ex_08.py

--- Lista_loops/ex_08.py
+++ Lista_loops/ex_08.py
@@ -1,13 +1,23 @@
 notas = [ ]
+
 nota = 1
+
 while nota != 0:
+     
      nota =(float(input(f"Digite o valor da nota:")))
+     
      notas.append(nota)
+
 soma = 0
+
 for nota in notas:
+    
     soma = soma + nota
+
 notas=notas[:-1]
+
 media = soma / len(notas)
+
 print(f"O valor da média é {round(media,2)}")
 
 
