0b6d444cf4fb258f221ce89dfe5191700b5a9a52
Douglas Freitas <freitas28douglas@gmail.com>
2022-04-15 00:38:35 -0300

    Update ex_6.py

--- Lista_loops/ex_6.py
+++ Lista_loops/ex_6.py
@@ -1,9 +1,17 @@
 peso = int(input('Informe o peso: '))
+
 peso_inválido = 0
+
 while peso != 0:
+    
     peso = int(input('Informe o peso novamente: '))
+    
     if peso < 100 or peso > 900:
+        
         peso_inválido += 1
+
 while peso == 0:
+    
     print('Encerrando, {} pesos inválidos.'.format(peso_inválido))
+    
     break
