e5b8d0be7eda08a13a295651b0cef7a36ea9030d
Douglas Freitas <freitas28douglas@gmail.com>
2022-04-15 00:36:49 -0300

    up

--- Lista_loops/ex_02.py
+++ Lista_loops/ex_02.py
@@ -1,7 +1,13 @@
 nota = float(input('Informe o valor da nota: '))
+
 contador = 0
+
 while nota != 0 and contador <= 8:
+    
     nota = float(input('Informe o valor da nota: '))
+    
     contador += 1
+
 if nota == 0:
+    
     print('Não aprovado')
