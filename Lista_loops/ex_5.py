b9becbc08ad6465f3106b42b83b3ac5dbdbbe203
Douglas Freitas <freitas28douglas@gmail.com>
2022-04-15 00:38:04 -0300

    Update ex_5.py

--- Lista_loops/ex_5.py
+++ Lista_loops/ex_5.py
@@ -1,11 +1,19 @@
 val  = 1
+
 while val != 0:
+    
     val = int(input('Digite um numero diferente de 0: '))
+    
     if val ==0:
+        
         print('Zero digitado, finalizando o programa')
+    
     elif val % 2 ==0:
+        
         print('Seu numero é par')
+    
     else:
+        
         print('Seu numero é impar')
 
     
