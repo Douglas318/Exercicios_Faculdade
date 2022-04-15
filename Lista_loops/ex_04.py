688cecb421f2c09ee09150818cd548a64e0a9510
Douglas Freitas <freitas28douglas@gmail.com>
2022-04-15 00:37:37 -0300

    Update ex_04.py

--- Lista_loops/ex_04.py
+++ Lista_loops/ex_04.py
@@ -1,6 +1,9 @@
 numero = int(input('Informe um número para a tabuada: '))
+
 for multi in range(1, 11):
+    
     tabuada = multi * numero
+    
     print('{} X {} = {}'.format(numero, multi, tabuada))
 
 
